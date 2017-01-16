c39bc815bd21348bf0ec42d34aa8d19eb8f6b033
Dimitris Bousis <whantana@gmail.com>
2017-01-16 16:46:23 +0200

    Removing eeg-hadoop-fft

--- eeg-hadoop-fft/data/curl_tsd.sh
+++
@@ -1,6 +0,0 @@
-export METRIC="Fp1"
-export SID="S001"
-export RID="R01"
-
-curl "http://centos-master:4242/q?start=2009/08/14-19:15:51.402&end=2009/08/14-19:15:51.668&m=sum:$METRIC\{sid=$SID,rid=$RID\}&ascii&exact" -o $METRIC"_"$SID"_"$RID.txt
-curl "http://centos-master:4242/q?start=2009/08/14-19:15:51.402&end=2009/08/14-19:15:51.668&m=sum:$METRIC\{sid=$SID,rid=$RID\}&json&exact" -o $METRIC"_"$SID"_"$RID.json
